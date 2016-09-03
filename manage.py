f8a33ad0e2bda403c495ee8b1ec52362117e9eda
neo1218 <neo1218@yeah.net>
2016-09-03 14:41:21 +0800

    fix tab error

--- manage.py
+++ manage.py
@@ -8,6 +8,6 @@ monkey.patch_all()
 
 
 if __name__ == '__main__':
-	# 每15分钟300次请求
-	grade = ratelimit.RateLimit(app, limit=300, per=60*15)
-	WSGIServer(('localhost', 8090), grade).serve_forever()
+    # 每15分钟300次请求限制
+    grade = ratelimit.RateLimit(app, limit=300, per=60*15)
+    WSGIServer(('localhost', 8090), grade).serve_forever()
--- middlewares/ratelimit.py
+++ middlewares/ratelimit.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 
 """
-	ratelimit.py
-	````````````
+    ratelimit.py
+    ````````````
 
-	api ratelimit middleware
+    api ratelimit middleware
 """
 
 import time
@@ -16,33 +16,33 @@ rate = redis.StrictRedis(host='localhost', port=6386, db=0) # for rate limit
 
 class RateLimit(object):
 
-	def __init__(self, app, limit, per): # send_x_headers):
-		self.app = app  # wsgi app
+    def __init__(self, app, limit, per): # send_x_headers):
+        self.app = app  # wsgi app
 
-		self.reset = int(time.time()) + per
-		self.limit = limit
-		self.per = per
-		# self.send_x_headers = send_x_headers
+        self.reset = int(time.time()) + per
+        self.limit = limit
+        self.per = per
+        # self.send_x_headers = send_x_headers
 
-	def __call__(self, environ, start_response):
-		"""
-		add ratelimit for all apis
-		"""
-		uip = environ.get('REMOTE_ADDR')
-		url = environ.get('PATH_INFO')
-		key_prefix = 'ratelimit/%s/%s' % (uip, url)
-		self.key = key_prefix + str(self.reset)
+    def __call__(self, environ, start_response):
+        """
+        add ratelimit for all apis
+        """
+        uip = environ.get('REMOTE_ADDR')
+        url = environ.get('PATH_INFO')
+        key_prefix = 'ratelimit/%s/%s' % (uip, url)
+        self.key = key_prefix + str(self.reset)
 
-		pipe = rate.pipeline()
-		pipe.incr(self.key)
-		pipe.expireat(self.key, self.reset)
+        pipe = rate.pipeline()
+        pipe.incr(self.key)
+        pipe.expireat(self.key, self.reset)
 
-		self.current = pipe.execute()[0]
-		self.remaining = self.limit - self.current
-		self.over_limit = self.current >= self.limit
+        self.current = pipe.execute()[0]
+        self.remaining = self.limit - self.current
+        self.over_limit = self.current >= self.limit
 
-		if self.over_limit:
-			start_response('400 Bad Request', 
-						 [('Content-Type', 'application/json')])
-			return []
-		return self.app(environ, start_response)
+        if self.over_limit:
+            start_response('400 Bad Request', 
+                         [('Content-Type', 'application/json')])
+            return []
+        return self.app(environ, start_response)
