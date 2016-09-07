4aeb9fc1b9d407f8dd09d98e2af44f59b8a473c5
neo1218 <neo1218@yeah.net>
2016-09-07 14:20:05 +0800

    update manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+import sys
 from grades import app
 from middlewares import ratelimit
 from gevent.wsgi import WSGIServer
@@ -9,5 +10,11 @@ monkey.patch_all()
 
 if __name__ == '__main__':
     # 每15分钟300次请求限制
+    host = 'localhost'
+    port = 8090
+    if len(sys.argv) > 1:
+        host = sys.argv[1]
+        port = int(sys.argv[2])
     grade = ratelimit.RateLimit(app, limit=300, per=60*15)
-    WSGIServer(('localhost', 8090), grade).serve_forever()
+    # WSGIServer(('localhost', 8090), grade).serve_forever()
+    WSGIServer((host, port), grade).serve_forever()
