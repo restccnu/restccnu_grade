f293ce1ff4a6026d867a23c436819ba3143e1702
neo1218 <neo1218@yeah.net>
2016-09-15 16:06:00 +0800

    update coroutx, app firing...

--- manage.py
+++ manage.py
@@ -10,12 +10,8 @@ monkey.patch_all()
 
 if __name__ == '__main__':
     # 每15分钟300次请求限制
-    host = 'localhost'
-    port = 8090
+    host = 'localhost'; port = '8090'
     if len(sys.argv) > 1:
-        host = sys.argv[1]
-        port = int(sys.argv[2])
-    # grade = ratelimit.RateLimit(app, limit=300, per=60*5)
-    # WSGIServer(('localhost', 8090), grade).serve_forever()
-    # WSGIServer((host, port), grade).serve_forever()
-    WSGIServer((host, port), app).serve_forever()
+        host = sys.argv[1] 
+        port = sys.argv[2]
+    app.firing(host=host, port=int(port)) #, debug=True)
